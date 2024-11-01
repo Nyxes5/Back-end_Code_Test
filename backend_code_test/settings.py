a0a3e56bc21d6f139ee51032e7629fe9f2d255a7
Jitse Willaert <jitse.willaert@telenet.be>
2024-11-01 13:22:41 +0100

    fix: refactor

--- backend_code_test/settings.py
+++ backend_code_test/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'persons',
+    'app',
     'rest_framework_swagger',
     'rest_framework',
     'drf_yasg'
